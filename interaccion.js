db7a2add79bede6a69109247292bacaa32273454
HelloAnaDev <hello.ana.dev.com>
2026-02-27 23:51:11 +0100

    La cabecera ha sido actualizada para tener un diseño responsive que sea funcional y se adapte visualmente a cualquier tamaño de pantalla

--- interaccion.js
+++ interaccion.js
@@ -1,1 +1,35 @@
 //js
+
+const btnMenu = document.getElementById('btnMenu');
+const menuLateral = document.getElementById('menuLateral');
+const cabecera = document.querySelector('.cabecera');
+
+btnMenu.addEventListener('click', () =>{
+    menuLateral.classList.toggle('activo');
+});
+
+
+
+
+let ubicacionPrincipal = window.scrollY;
+    window.addEventListener('scroll', () => {
+        let desplazamientoActual = window.scrollY;
+        if (menuLateral.classList.contains('activo')){
+            return;
+        }
+        if (ubicacionPrincipal>=desplazamientoActual){
+            cabecera.classList.remove('oculta');
+        } else {
+            cabecera.classList.add('oculta');
+        }
+        ubicacionPrincipal=desplazamientoActual
+    })
+
+
+
+    
+const bntOcultar = document.getElementById('btnOcultar');
+
+bntOcultar.addEventListener('click',() =>{
+    menuLateral.classList.remove('activo');
+})
